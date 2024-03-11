3d16fcf23b402ddc31d469f695d881083a7e4a6b
Asia <2736300+humpydonkey@users.noreply.github.com>
2024-03-11 13:24:23 +0800

    Implement the Grounding DINO tool (#12)
    * Implement Grounding DINO tool
    
    * error handling
    
    * Fix typing errors
    
    ---------
    
    Co-authored-by: Yazhou Cao <yazhou.cao@landing.ai>

--- vision_agent/tools/tools.py
+++ vision_agent/tools/tools.py
@@ -1,7 +1,14 @@
-from typing import Dict, List, Union
+import logging
 from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Any, Dict, List, Union, cast
 
+import requests
 from PIL.Image import Image as ImageType
+
+from vision_agent.image_utils import convert_to_b64
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class ImageTool(ABC):
@@ -27,6 +34,8 @@ class CLIP(ImageTool):
 
 
 class GroundingDINO(ImageTool):
+    _ENDPOINT = "https://chnicr4kes5ku77niv2zoytggq0qyqlp.lambda-url.us-east-2.on.aws"
+
     doc = (
         "Grounding DINO is a tool that can detect arbitrary objects with inputs such as category names or referring expressions."
         "Here are some exmaples of how to use the tool, the examples are in the format of User Question: which will have the user's question in quotes followed by the parameters in JSON format, which is the parameters you need to output to call the API to solve the user's question.\n"
@@ -38,8 +47,21 @@ class GroundingDINO(ImageTool):
     def __init__(self, prompt: str):
         self.prompt = prompt
 
-    def __call__(self, image: Union[str, ImageType]) -> List[Dict]:
-        raise NotImplementedError
+    def __call__(self, image: Union[str, Path, ImageType]) -> List[Dict]:
+        image_b64 = convert_to_b64(image)
+        data = {
+            "prompt": self.prompt,
+            "images": [image_b64],
+        }
+        res = requests.post(
+            self._ENDPOINT,
+            headers={"Content-Type": "application/json"},
+            json=data,
+        )
+        resp_json: Dict[str, Any] = res.json()
+        if resp_json["statusCode"] != 200:
+            _LOGGER.error(f"Request failed: {resp_json}")
+        return cast(List[Dict], resp_json["data"])
 
 
 class GroundingSAM(ImageTool):
