c064c88b0f160e3a7a5ba71e5f0f95eb90138235
wuyiqunLu <132986242+wuyiqunLu@users.noreply.github.com>
2024-05-23 00:56:49 -0500

    feat: add progress logging for v2 and v3 (#93)
    * feat: add progress loging for v3
    
    * fix lint
    
    * fix lint
    
    * fix format
    
    * fix format
    
    * fix format
    
    * run black formatter
    
    * fix lint
    
    * fix lint
    
    * fix lint
    
    * separate code and test
    
    * add reflection

--- vision_agent/agent/vision_agent_v3.py
+++ vision_agent/agent/vision_agent_v3.py
@@ -3,7 +3,7 @@ import json
 import logging
 import sys
 from pathlib import Path
-from typing import Any, Dict, List, Optional, Union, cast
+from typing import Any, Dict, List, Optional, Union, cast, Callable
 
 from rich.console import Console
 from rich.syntax import Syntax
@@ -114,6 +114,7 @@ def write_and_test_code(
     coder: LLM,
     tester: LLM,
     debugger: LLM,
+    log_progress: Callable[[Dict[str, Any]], None],
     verbosity: int = 0,
     max_retries: int = 3,
 ) -> Dict[str, Any]:
@@ -131,8 +132,26 @@ def write_and_test_code(
     success, result = _EXECUTE.run_isolation(f"{code}\n{test}")
     if verbosity == 2:
         _LOGGER.info("First code and tests:")
+        log_progress(
+            {
+                "log": "Code:",
+                "code": code,
+            }
+        )
+        log_progress(
+            {
+                "log": "Test:",
+                "code": test,
+            }
+        )
         _CONSOLE.print(
             Syntax(f"{code}\n{test}", "python", theme="gruvbox-dark", line_numbers=True)
+        )
+        log_progress(
+            {
+                "log": "Result:",
+                "result": result,
+            }
         )
         _LOGGER.info(f"First result: {result}")
 
@@ -156,6 +175,12 @@ def write_and_test_code(
 
         success, result = _EXECUTE.run_isolation(f"{code}\n{test}")
         if verbosity == 2:
+            log_progress(
+                {
+                    "log": f"Debug attempt {count + 1}, reflection:",
+                    "result": fixed_code_and_test["reflections"],
+                }
+            )
             _LOGGER.info(
                 f"Debug attempt {count + 1}, reflection: {fixed_code_and_test['reflections']}"
             )
@@ -163,6 +188,12 @@ def write_and_test_code(
                 Syntax(
                     f"{code}\n{test}", "python", theme="gruvbox-dark", line_numbers=True
                 )
+            )
+            log_progress(
+                {
+                    "log": "Debug result:",
+                    "result": result,
+                }
             )
             _LOGGER.info(f"Debug result: {result}")
         count += 1
@@ -182,7 +213,10 @@ def write_and_test_code(
 
 
 def retrieve_tools(
-    plan: List[Dict[str, str]], tool_recommender: Sim, verbosity: int = 0
+    plan: List[Dict[str, str]],
+    tool_recommender: Sim,
+    log_progress: Callable[[Dict[str, Any]], None],
+    verbosity: int = 0,
 ) -> str:
     tool_info = []
     tool_desc = []
@@ -191,6 +225,12 @@ def retrieve_tools(
         tool_info.extend([e["doc"] for e in tools])
         tool_desc.extend([e["desc"] for e in tools])
     if verbosity == 2:
+        log_progress(
+            {
+                "log": "Retrieved tools:",
+                "tools": tool_desc,
+            }
+        )
         _LOGGER.info(f"Tools: {tool_desc}")
     tool_info_set = set(tool_info)
     return "\n\n".join(tool_info_set)
@@ -206,6 +246,7 @@ class VisionAgentV3(Agent):
         debugger: Optional[LLM] = None,
         tool_recommender: Optional[Sim] = None,
         verbosity: int = 0,
+        report_progress_callback: Optional[Callable[[Dict[str, Any]], None]] = None,
     ) -> None:
         self.planner = (
             OpenAILLM(temperature=0.0, json_mode=True) if planner is None else planner
@@ -223,6 +264,7 @@ class VisionAgentV3(Agent):
         )
         self.verbosity = verbosity
         self.max_retries = 3
+        self.report_progress_callback = report_progress_callback
 
     def __call__(
         self,
@@ -261,6 +303,12 @@ class VisionAgentV3(Agent):
             )
             plan_i_str = "\n-".join([e["instructions"] for e in plan_i])
             if self.verbosity == 1 or self.verbosity == 2:
+                self.log_progress(
+                    {
+                        "log": "Going to run the following plan(s) in sequence:\n",
+                        "plan": plan_i,
+                    }
+                )
                 _LOGGER.info(
                     f"""
 {tabulate(tabular_data=plan_i, headers="keys", tablefmt="mixed_grid", maxcolwidths=_MAX_TABULATE_COL_WIDTH)}"""
@@ -269,6 +317,7 @@ class VisionAgentV3(Agent):
             tool_info = retrieve_tools(
                 plan_i,
                 self.tool_recommender,
+                self.log_progress,
                 self.verbosity,
             )
             results = write_and_test_code(
@@ -279,6 +328,7 @@ class VisionAgentV3(Agent):
                 self.coder,
                 self.tester,
                 self.debugger,
+                self.log_progress,
                 verbosity=self.verbosity,
             )
             success = cast(bool, results["success"])
@@ -289,10 +339,23 @@ class VisionAgentV3(Agent):
 
             reflection = reflect(chat, plan_i_str, code, self.planner)
             if self.verbosity > 0:
+                self.log_progress(
+                    {
+                        "log": "Reflection:",
+                        "reflection": reflection,
+                    }
+                )
                 _LOGGER.info(f"Reflection: {reflection}")
             feedback = cast(str, reflection["feedback"])
             success = cast(bool, reflection["success"])
             working_memory.append({"code": f"{code}\n{test}", "feedback": feedback})
+
+        self.log_progress(
+            {
+                "log": f"The Vision Agent V3 has concluded this chat.\nSuccess: {success}",
+                "finished": True,
+            }
+        )
 
         return {
             "code": code,
@@ -301,5 +364,7 @@ class VisionAgentV3(Agent):
             "working_memory": working_memory,
         }
 
-    def log_progress(self, description: str) -> None:
+    def log_progress(self, data: Dict[str, Any]) -> None:
+        if self.report_progress_callback is not None:
+            self.report_progress_callback(data)
         pass
