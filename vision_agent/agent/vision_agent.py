5ab9ac21dacce6484f9cf1fe302930ccd5259be2
Asia <2736300+humpydonkey@users.noreply.github.com>
2024-06-13 17:48:38 -0700

    Add logging to extract_json() (#135)

--- vision_agent/agent/vision_agent.py
+++ vision_agent/agent/vision_agent.py
@@ -97,6 +97,7 @@ def extract_json(json_str: str) -> Dict[str, Any]:
     try:
         json_dict = json.loads(json_str)
     except json.JSONDecodeError:
+        input_json_str = json_str
         if "```json" in json_str:
             json_str = json_str[json_str.find("```json") + len("```json") :]
             json_str = json_str[: json_str.find("```")]
@@ -104,7 +105,12 @@ def extract_json(json_str: str) -> Dict[str, Any]:
             json_str = json_str[json_str.find("```") + len("```") :]
             # get the last ``` not one from an intermediate string
             json_str = json_str[: json_str.find("}```")]
-        json_dict = json.loads(json_str)
+        try:
+            json_dict = json.loads(json_str)
+        except json.JSONDecodeError as e:
+            error_msg = f"Could not extract JSON from the given str: {json_str}.\nFunction input:\n{input_json_str}"
+            _LOGGER.exception(error_msg)
+            raise ValueError(error_msg) from e
     return json_dict  # type: ignore
 
 
