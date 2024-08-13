8b23c53de63a90e6955dead362a00cfaeb14d6b5
Dillon Laird <dillonalaird@gmail.com>
2024-08-13 13:51:44 -0700

    Add Streaming for LMMs (#191)
    * added streaming
    
    * fixed type errors
    
    * fixed linting
    
    * fixed generator func type
    
    * black formatting
    
    * fixed tests for streaming

--- tests/unit/fixtures.py
+++ tests/unit/fixtures.py
@@ -13,11 +13,21 @@ def langsmith_wrap_oepnai_mock(request, openai_llm_mock):
 @pytest.fixture
 def openai_lmm_mock(request):
     content = request.param
+
+    def mock_generate(*args, **kwargs):
+        if kwargs.get("stream", False):
+
+            def generator():
+                for chunk in content.split(" ") + [None]:
+                    yield MagicMock(choices=[MagicMock(delta=MagicMock(content=chunk))])
+
+            return generator()
+        else:
+            return MagicMock(choices=[MagicMock(message=MagicMock(content=content))])
+
     # Note the path here is adjusted to where OpenAI is used, not where it's defined
     with patch("vision_agent.lmm.lmm.OpenAI") as mock:
         # Setup a mock response structure that matches what your code expects
         mock_instance = mock.return_value
-        mock_instance.chat.completions.create.return_value = MagicMock(
-            choices=[MagicMock(message=MagicMock(content=content))]
-        )
+        mock_instance.chat.completions.create.return_value = mock_generate()
         yield mock_instance
