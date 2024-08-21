2c482f95c73b8ef9ed0a33d3153b513af4527919
Dillon Laird <dillonalaird@gmail.com>
2024-08-20 17:09:36 -0700

    don't print multiple plans if no multi-plan

--- vision_agent/agent/vision_agent_coder.py
+++ vision_agent/agent/vision_agent_coder.py
@@ -734,20 +734,21 @@ class VisionAgentCoder(Agent):
                 self.planner,
             )
 
-            if self.verbosity >= 1:
-                for p in plans:
-                    # tabulate will fail if the keys are not the same for all elements
-                    p_fixed = [
-                        {
-                            "instructions": (
-                                e["instructions"] if "instructions" in e else ""
-                            )
-                        }
-                        for e in plans[p]
-                    ]
-                    _LOGGER.info(
-                        f"\n{tabulate(tabular_data=p_fixed, headers='keys', tablefmt='mixed_grid', maxcolwidths=_MAX_TABULATE_COL_WIDTH)}"
-                    )
+            if test_multi_plan:
+                if self.verbosity >= 1:
+                    for p in plans:
+                        # tabulate will fail if the keys are not the same for all elements
+                        p_fixed = [
+                            {
+                                "instructions": (
+                                    e["instructions"] if "instructions" in e else ""
+                                )
+                            }
+                            for e in plans[p]
+                        ]
+                        _LOGGER.info(
+                            f"\n{tabulate(tabular_data=p_fixed, headers='keys', tablefmt='mixed_grid', maxcolwidths=_MAX_TABULATE_COL_WIDTH)}"
+                        )
 
             tool_infos = retrieve_tools(
                 plans,
