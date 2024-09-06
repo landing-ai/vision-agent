b082c016bb201fb3094034430cbb6d81e79d2b60
Dillon Laird <dillonalaird@gmail.com>
2024-09-05 18:11:52 -0700

    Update video reader (#226)
    * updated video reader
    
    * updated dependencies
    
    * organized video writing
    
    * flake8
    
    * fix type error
    
    * fix type error
    
    * fix test case
    
    * add warning comment
    
    * fix print

--- tests/unit/tools/test_video.py
+++ tests/unit/tools/test_video.py
@@ -4,5 +4,7 @@ from vision_agent.utils.video import extract_frames_from_video
 def test_extract_frames_from_video():
     # TODO: consider generating a video on the fly instead
     video_path = "tests/data/video/test.mp4"
+
+    # there are 48 frames at 24 fps in this video file
     res = extract_frames_from_video(video_path)
-    assert len(res) == 1
+    assert len(res) == 2
