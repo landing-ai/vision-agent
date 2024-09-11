52bebe547144a35ede59143b785f5fd27fb8bfbf
Dillon Laird <dillonalaird@gmail.com>
2024-09-10 20:15:49 -0700

    fix test case

--- tests/unit/tools/test_video.py
+++ tests/unit/tools/test_video.py
@@ -6,5 +6,5 @@ def test_extract_frames_from_video():
     video_path = "tests/data/video/test.mp4"
 
     # there are 48 frames at 24 fps in this video file
-    res = extract_frames_from_video(video_path)
-    assert len(res) == 2
+    res = extract_frames_from_video(video_path, fps=24)
+    assert len(res) == 48
