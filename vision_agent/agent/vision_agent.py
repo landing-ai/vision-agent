e0075c7a3fcabdcc7bab3a7d69687df1948eeb4f
Dillon Laird <dillonalaird@gmail.com>
2024-10-02 11:48:51 -0700

    fixed circular imports

--- vision_agent/agent/vision_agent.py
+++ vision_agent/agent/vision_agent.py
@@ -14,8 +14,8 @@ from vision_agent.agent.vision_agent_prompts import (
     VA_CODE,
 )
 from vision_agent.lmm import LMM, AnthropicLMM, Message, OpenAILMM
-from vision_agent.tools import META_TOOL_DOCSTRING
 from vision_agent.tools.meta_tools import (
+    META_TOOL_DOCSTRING,
     Artifacts,
     check_and_load_image,
     use_extra_vision_agent_args,
