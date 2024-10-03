f317fc31563ff9502f4638c92f5152296e5e92dc
Dillon Laird <dillonalaird@gmail.com>
2024-10-03 16:46:39 -0700

    revert changes with planning step for now

--- vision_agent/agent/vision_agent.py
+++ vision_agent/agent/vision_agent.py
@@ -233,7 +233,7 @@ class VisionAgent(Agent):
         input: Union[str, List[Message]],
         media: Optional[Union[str, Path]] = None,
         artifacts: Optional[Artifacts] = None,
-    ) -> List[Message]:
+    ) -> str:
         """Chat with VisionAgent and get the conversation response.
 
         Parameters:
@@ -250,10 +250,28 @@ class VisionAgent(Agent):
             input = [{"role": "user", "content": input}]
             if media is not None:
                 input[0]["media"] = [media]
-        results, _ = self.chat_with_code(input, artifacts)
-        return results
+        results, _ = self.chat_and_artifacts(input, artifacts)
+        return results[-1]["content"]  # type: ignore
 
-    def chat_with_code(
+    def chat(
+        self,
+        chat: List[Message],
+    ) -> List[Message]:
+        """Chat with VisionAgent, it will use code to execute actions to accomplish
+        its tasks.
+
+        Parameters:
+            chat (List[Message]): A conversation in the format of:
+                [{"role": "user", "content": "describe your task here..."}]
+                or if it contains media files, it should be in the format of:
+                [{"role": "user", "content": "describe your task here...", "media": ["image1.jpg", "image2.jpg"]}]
+
+        Returns:
+            List[Message]: The conversation response.
+        """
+        return self.chat_and_artifacts(chat)[0]
+
+    def chat_and_artifacts(
         self,
         chat: List[Message],
         artifacts: Optional[Artifacts] = None,
