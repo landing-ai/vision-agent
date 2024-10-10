8e57b8f4df8be4e057af12d0720cdb10bf7f3f0f
Dillon Laird <dillonalaird@gmail.com>
2024-10-10 07:39:48 -0700

    new updates to planner v2

--- vision_agent/agent/vision_agent_planner_v2.py
+++ vision_agent/agent/vision_agent_planner_v2.py
@@ -17,6 +17,7 @@ from vision_agent.agent.agent_utils import (
 from vision_agent.agent.vision_agent_planner_prompts import (
     EXAMPLE_PLAN1,
     FINALIZE_PLAN,
+    FIX_BUG,
     PLAN2,
 )
 from vision_agent.lmm import LMM, AnthropicLMM, Message
@@ -26,12 +27,28 @@ from vision_agent.utils.execute import (
     Execution,
 )
 
-logging.basicConfig(stream=sys.stdout)
+logging.basicConfig(level=logging.INFO)
 _LOGGER = logging.getLogger(__name__)
 READ_DOCSTRING = T.get_tool_documentation(
     [T.load_image, T.extract_frames_and_timestamps]
 )
 PLANNING_TOOLS_DOCSTRING = READ_DOCSTRING + "\n" + pt.PLANNER_DOCSTRING
+
+
+class DefaultPlanningImports:
+    imports = [
+        "import os",
+        "import numpy as np",
+        "from typing import *",
+        "from vision_agent.tools import *",
+        "from vision_agent.tools.planner_tools import claude35_vqa, get_tool_for_task",
+        "from pillow_heif import register_heif_opener",
+        "register_heif_opener()",
+    ]
+
+    @staticmethod
+    def prepend_imports(code: str) -> str:
+        return "\n".join(DefaultPlanningImports.imports) + "\n\n" + code
 
 
 def get_planning(
@@ -67,12 +84,44 @@ def run_planning(
     return response
 
 
+def code_safeguards(code: str) -> str:
+    if "get_tool_for_task" in code:
+        lines = code.split("\n")
+        new_lines = []
+        for line in lines:
+            new_lines.append(line)
+            if "get_tool_for_task" in line:
+                break
+        code = "\n".join(new_lines)
+    return code
+
+
 def execute_code_action(
-    code: str, code_interpreter: CodeInterpreter
-) -> Tuple[Execution, str]:
-    execution = code_interpreter.exec_cell(code)
+    code: str, code_interpreter: CodeInterpreter, verbosity: int = 0
+) -> Tuple[Execution, str, str]:
+    execution = code_interpreter.exec_cell(DefaultPlanningImports.prepend_imports(code))
     obs = execution.text(include_results=False).strip()
-    return execution, obs
+    count = 1
+    if verbosity > 0:
+        print_code("Execute Code:", code)
+        _LOGGER.info(f"Code Execution Output: {obs}")
+
+    while not execution.success and count <= 3:
+        code = FIX_BUG.format(code=code, error=obs)
+        execution = code_interpreter.exec_cell(
+            DefaultPlanningImports.prepend_imports(code)
+        )
+        obs = execution.text(include_results=False).strip()
+        if verbosity > 0:
+            print_code(f"Execute Code Round {count}:", code)
+            _LOGGER.info(f"Code Execution Output: {obs}")
+        count += 1
+
+    if obs.startswith("----- stdout -----\n"):
+        obs = obs[19:]
+    if obs.endswith("\n----- stderr -----"):
+        obs = obs[:-19]
+    return execution, obs, code
 
 
 class VisionAgentPlannerV2(Agent):
@@ -89,6 +138,8 @@ class VisionAgentPlannerV2(Agent):
         self.max_steps = max_steps
         self.code_sandbox_runtime = code_sandbox_runtime
         self.verbosity = verbosity
+        if self.verbosity >= 1:
+            _LOGGER.setLevel(logging.INFO)
 
     def __call__(
         self,
@@ -116,7 +167,7 @@ class VisionAgentPlannerV2(Agent):
             orig_chat = copy.deepcopy(chat)
             int_chat = copy.deepcopy(chat)
             media_list = []
-            for chat_i in chat:
+            for chat_i in int_chat:
                 if "media" in chat_i:
                     for media in chat_i["media"]:
                         media = (
@@ -147,9 +198,11 @@ class VisionAgentPlannerV2(Agent):
             step = 0
             finished = False
             while step < self.max_steps or not finished:
-                response = run_planning(chat, self.planner)
-
                 __import__("ipdb").set_trace()
+                response = run_planning(int_chat, self.planner)
+                if self.verbosity > 1:
+                    _LOGGER.info(f"Response: {response}")
+
                 thinking = extract_thinking(response)
                 code = extract_execution(response)
                 finalize_plan = extract_finalize_plan(response)
@@ -160,27 +213,30 @@ class VisionAgentPlannerV2(Agent):
                 int_chat.append({"role": "assistant", "content": response})
                 orig_chat.append({"role": "assistant", "content": response})
 
+                if code is not None:
+                    code = code_safeguards(code)
+                    _, obs, code = execute_code_action(
+                        code, code_interpreter, self.verbosity
+                    )
+                    int_chat.append({"role": "observation", "content": obs})
+                    orig_chat.append({"role": "observation", "content": obs})
+
                 if finalize_plan is not None:
                     _LOGGER.info(f"Finalizing plan: {finalize_plan}")
                     break
 
-                if code is not None:
-                    print_code("Execute Code:", code)
-                    _, obs = execute_code_action(code, code_interpreter)
-                    if self.verbosity > 0:
-                        _LOGGER.info(f"Code Execution: {obs}")
-                    int_chat.append({"role": "observation", "content": obs})
-                    orig_chat.append({"role": "observation", "content": obs})
-
                 step += 1
 
             prompt = FINALIZE_PLAN.format(
-                user_request=chat[-1]["content"],
-                planning=get_planning(chat),
+                planning=get_planning(int_chat),
+                excluded_tools=str([t.__name__ for t in pt.PLANNER_TOOLS]),
             )
 
+            __import__("ipdb").set_trace()
             plan_str = cast(str, self.planner.generate(prompt))
             plan = extract_json(plan_str)
+            if self.verbosity > 0:
+                _LOGGER.info(f"Final Plan: {plan}")
 
         return plan
 
