5e48467c337b51290ec3b7cddc508a1aacca6669
Coby Zhong <coby.zhong@landing.ai>
2024-11-21 09:12:09 -0800

    other: initial setup of page setup

--- examples/chat/chat-app-v2/src/app/page.tsx
+++ examples/chat/chat-app-v2/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 
+import Chat from "./chat/components/ChatWrapper";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        {/* TODO: Include chat component here */}
+    <div className="h-screen py-8 gap-4 font-[family-name:var(--font-geist-sans)]">
+      <main className="relative flex flex-col size-full gap-8 items-center sm:items-start overflow-x-hidden">
+        <Chat />
       </main>
     </div>
   );
