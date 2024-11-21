c937af6b620be0f873267b508989eae1f822aa14
Coby Zhong <coby.zhong@landing.ai>
2024-11-21 13:09:00 -0800

    config: allow localhost pictures for mock data purposes

--- examples/chat/chat-app-v2/next.config.ts
+++ examples/chat/chat-app-v2/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
