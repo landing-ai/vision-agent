1c40500edfb9582d8f64953e6dc911b80dd4d575
Coby Zhong <coby.zhong@landing.ai>
2024-11-21 13:24:17 -0800

    fix: overflow bug with right-side panel

--- examples/chat/chat-app-v2/src/app/chat/components/ResultsPanel.tsx
+++ examples/chat/chat-app-v2/src/app/chat/components/ResultsPanel.tsx
@@ -1,6 +1,7 @@
-import { Button } from "@/components/ui/Button";
 import * as Tabs from "@radix-ui/react-tabs";
+import * as ToggleGroup from "@radix-ui/react-toggle-group";
 import Image from "next/image";
+import { useState } from "react";
 
 {/* TODO: Add a component which renders code and result images */}
 export interface ResultsPanelProps {
@@ -10,35 +11,50 @@ export interface ResultsPanelProps {
 }
 
 export default function ResultsPanel({ code, images, backButton }: ResultsPanelProps): JSX.Element {
-  // const [activeTab, setActiveTab]
+  const [activeTab, setActiveTab] = useState<string>("code");
 
   return (<div className="relative flex flex-col gap-2 h-full overflow-hidden">
     {backButton}
-    <Tabs.Root defaultValue="code" className="shrink grow flex flex-col gap-2">
-      <Tabs.List className="flex gap-2">
-        <Tabs.Trigger value="code" className="py-1 px-2 rounded bg-gray-700 data-[state=active]:bg-green-600">
+    <div className="flex flex-col gap-2 h-full overflow-hidden">
+      <ToggleGroup.Root
+        type="single"
+        defaultValue="code"
+        aria-label="Execution result tab"
+        className="flex gap-2"
+        onValueChange={(value) => {if (value) setActiveTab(value)}}>
+        <ToggleGroup.Item value="code" className="py-1 px-2 rounded bg-gray-700 data-[state=on]:bg-green-600">
           Code
-        </Tabs.Trigger>
-        <Tabs.Trigger value="result" className="py-1 px-2 rounded bg-gray-700 data-[state=active]:bg-green-600">
+        </ToggleGroup.Item>
+        <ToggleGroup.Item value="result" className="py-1 px-2 rounded bg-gray-700 data-[state=on]:bg-green-600">
           Result
-        </Tabs.Trigger>
-      </Tabs.List>
-      <Tabs.Content value="code" className="shrink h-full overflow-hidden">
-        <pre className="overflow-y-auto h-full">
-          <code>
-            {code}
-          </code>
-        </pre>
-      </Tabs.Content>
-      <Tabs.Content value="result" className="shrink overflow-hidden h-full">
-        {images?.map((image, index) =>
-          <Image
-            src={image}
-            alt={`Processed image ${index}`}
-            key={index}
-            fill
-            className={"object-contain"} />)}
-      </Tabs.Content>
-    </Tabs.Root>
+        </ToggleGroup.Item>
+      </ToggleGroup.Root>
+      <Tabs.Root value={activeTab} className="flex-1 overflow-hidden">
+        {/* <Tabs.List className="flex gap-2">
+          <Tabs.Trigger value="code" className="py-1 px-2 rounded bg-gray-700 data-[state=active]:bg-green-600">
+            Code
+          </Tabs.Trigger>
+          <Tabs.Trigger value="result" className="py-1 px-2 rounded bg-gray-700 data-[state=active]:bg-green-600">
+            Result
+          </Tabs.Trigger>
+        </Tabs.List> */}
+        <Tabs.Content value="code" className="shrink h-full overflow-y-auto">
+          <pre className="rounded-sm bg-gray-800 p-2">
+            <code>
+              {code}
+            </code>
+          </pre>
+        </Tabs.Content>
+        <Tabs.Content value="result" className="relative shrink h-full overflow-y-auto">
+          {images?.map((image, index) =>
+            <Image
+              src={image}
+              alt={`Processed image ${index}`}
+              key={index}
+              fill
+              className={"object-contain"} />)}
+        </Tabs.Content>
+      </Tabs.Root>
+    </div>
   </div>)
 }
