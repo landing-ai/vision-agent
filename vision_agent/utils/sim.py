b81ba9e8f21d89dec22cc8770202401443def57c
Dillon Laird <dillonalaird@gmail.com>
2025-01-03 15:34:21 -0800

    formatting fix

--- vision_agent/utils/sim.py
+++ vision_agent/utils/sim.py
@@ -133,7 +133,10 @@ class Sim:
         df: pd.DataFrame,
     ) -> bool:
         load_dir = Path(load_dir)
-        if not Path(load_dir / "df.csv").exists() or Path(load_dir / "embs.npy").exists():
+        if (
+            not Path(load_dir / "df.csv").exists()
+            or Path(load_dir / "embs.npy").exists()
+        ):
             return False
 
         df_load = pd.read_csv(load_dir / "df.csv")
