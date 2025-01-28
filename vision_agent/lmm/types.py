6dd9ffdf7fa6631252be892be4367f9305a9b23f
Dillon Laird <dillonalaird@gmail.com>
2025-01-28 09:41:09 -0800

    update types

--- vision_agent/lmm/types.py
+++ vision_agent/lmm/types.py
@@ -1,7 +1,10 @@
 from pathlib import Path
 from typing import Dict, Sequence, Union
 
+import numpy as np
+from PIL.Image import Image as ImageType
+
 from vision_agent.utils.execute import Execution
 
-TextOrImage = Union[str, Sequence[Union[str, Path]]]
+TextOrImage = Union[str, Sequence[Union[str, Path, ImageType, np.ndarray]]]
 Message = Dict[str, Union[TextOrImage, Execution]]
