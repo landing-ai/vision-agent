b508b162cda1b089a2af73f3e98e31326794ae69
Cameron Maloney <cameron.maloney@warriorlife.net>
2025-04-28 13:12:10 -0700

    Use version of pytube that does not give 400 error

--- vision_agent/tools/tools.py
+++ vision_agent/tools/tools.py
@@ -19,7 +19,7 @@ import requests
 from IPython.display import display
 from PIL import Image, ImageDraw, ImageFont
 from pillow_heif import register_heif_opener  # type: ignore
-from pytube import YouTube  # type: ignore
+from pytubefix import YouTube  # type: ignore
 import pymupdf  # type: ignore
 from google import genai  # type: ignore
 from google.genai import types  # type: ignore
